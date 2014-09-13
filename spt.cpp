cee5be11b127d7e025016ecb7f2436c4794a7eaf
Fazlul Shahriar <fshahriar@gc.cuny.edu>
2014-09-12 22:08:33 -0400

    add some filters and start on resampling

--- spt.cpp
+++ spt.cpp
@@ -14,16 +14,23 @@ ncfatal(int n)
 	exit(2);
 }
 
-Mat
-readsst(void)
+void
+fatal(string msg)
 {
-	int i, ncid, varid, n, dimids[2];
+	cerr << msg << endl;
+	exit(2);
+}
+
+Mat
+readvar(int ncid, const char *name)
+{
+	int i, varid, n, dimids[2];
 	size_t shape[2];
-	Mat sst;
+	Mat img;
 	
 	if(n = nc_open(DATAPATH, NC_NOWRITE, &ncid))
 		ncfatal(n);
-	if(n = nc_inq_varid(ncid, "sst_regression", &varid))
+	if(n = nc_inq_varid(ncid, name, &varid))
 		ncfatal(n);
 	if(n = nc_inq_vardimid(ncid, varid, dimids))
 		ncfatal(n);
@@ -31,54 +38,150 @@ readsst(void)
 		if(n = nc_inq_dimlen(ncid, dimids[i], &shape[i]))
 			ncfatal(n);
 	}
-	sst = Mat::zeros(shape[0], shape[1], CV_32FC1);
-	if(n = nc_get_var_float(ncid, varid, (float*)sst.data))
+	img = Mat::zeros(shape[0], shape[1], CV_32FC1);
+	if(n = nc_get_var_float(ncid, varid, (float*)img.data))
 		ncfatal(n);
-	if(n = nc_close(ncid))
-		ncfatal(n);
-
-	return sst;
+	return img;
 }
 
 void
-cmapImshow(Mat &img, int cmap, double scale)
+cmapImshow(string name, Mat &img, int cmap, double scale)
 {
-	int i, key;
 	double min, max;
-	float *p;
-	Mat tmp, tmp2, image;
 
 	minMaxLoc(img, &min, &max, NULL, NULL);
-	i = 0;
-	p = (float*)img.data;
-	for(i = 0; i < img.total(); i++){
-		p[i] -= min;
-		p[i] /= max-min;
-		p[i] *= 255;
+	cout << name << " min: " << min << " max: " << max << endl;
+	cout << name << " type: " << img.type() << endl;
+
+	switch(img.type()){
+	case CV_16SC1:
+	case CV_32SC1:
+		img.convertTo(img, CV_64F);
+		// fallthrough
+	case CV_32FC1:
+	case CV_64FC1:
+		img -= min;
+		img.convertTo(img, CV_8U, 255.0/(max-min));
+		break;
 	}
+	//resize(img, tmp2, Size(), scale, scale);
+	applyColorMap(img, img, cmap);
 
-	img.convertTo(tmp, CV_8U);
-	resize(tmp, tmp2, Size(), scale, scale);
-	applyColorMap(tmp2, image, cmap);
+	//namedWindow(name, WINDOW_AUTOSIZE);
+	namedWindow(name, CV_WINDOW_NORMAL|CV_WINDOW_KEEPRATIO);
+	imshow(name, img);
+}
 
-	namedWindow("Display window", WINDOW_AUTOSIZE);
-	imshow("Display window", image);
-	do{
-		key = waitKey(0);
-	}while(key != 'q');
+void
+clipsst(Mat &sst)
+{
+	float *p;
+	int i;
+
+	p = (float*)sst.data;
+	for(i = 0; i < sst.total(); i++){
+		if(p[i] > 1000 || p[i] < -1000)
+			p[i] = NAN;
+	}
+}
+
+void
+laplacian(Mat &src, Mat &dst)
+{
+	 Mat kern = (Mat_<double>(3,3) <<
+	 	0,     1/4.0,  0,
+		1/4.0, -4/4.0, 1/4.0,
+		0,     1/4.0,  0);
+	filter2D(src, dst, -1, kern);
+}
+
+Mat
+unsort_float64(Mat &sind, Mat &img)
+{
+	Mat newimg;
+	int i, j, k;
+	int32_t *sp;
+	float *ip;
+
+	if(img.type() != CV_32FC1){
+		fatal("invalid img type");
+	}
+	newimg = Mat::zeros(img.rows, img.cols, CV_64F);
+	sp = (int32_t*)sind.data;
+	ip = (float*)img.data;
+	k = 0;
+	for(i = 0; i < newimg.rows; i++){
+		for(j = 0; j < newimg.cols; j++){
+			newimg.at<double>(sp[k], j) = ip[k];
+			k++;
+		}
+	}
+	return newimg;
+}
+
+Mat
+sort_float64(Mat &sind, Mat &img)
+{
+	Mat newimg;
+	int i, j, k;
+	int32_t *sp;
+	double *np;
+
+	if(img.type() != CV_32FC1){
+		fatal("invalid img type");
+	}
+	newimg = Mat::zeros(img.rows, img.cols, CV_64F);
+	sp = (int32_t*)sind.data;
+	np = (double*)newimg.data;
+	k = 0;
+	for(i = 0; i < newimg.rows; i++){
+		for(j = 0; j < newimg.cols; j++){
+			np[k] = img.at<float>(sp[k], j);
+			k++;
+		}
+	}
+	return newimg;
 }
 
 int
 main(int argc, char **argv)
 {
-	Mat sst, tmp, tmp2, image;
+	Mat sst, lat, elem, sstdil, sstero, rfilt, sstlap, sind;
+	float *p, *q;
+	int i, ncid, n;
 
-	sst = readsst();
+	if(n = nc_open(DATAPATH, NC_NOWRITE, &ncid))
+		ncfatal(n);
+	sst = readvar(ncid, "sst_regression");
+	lat = readvar(ncid, "latitude");
+	if(n = nc_close(ncid))
+		ncfatal(n);
+
 	if(sst.empty()){	// Check for invalid input
 		cout <<  "could not read data" << std::endl ;
 		return -1;
 	}
-	cmapImshow(sst, COLORMAP_JET, 0.2);
+
+	sortIdx(lat, sind, CV_SORT_EVERY_COLUMN + CV_SORT_ASCENDING);
+	sst = sort_float64(sind, sst);
+
+	elem = getStructuringElement(MORPH_RECT, Size(3, 3), Point(1, 1));
+	dilate(sst, sstdil, elem);
+	erode(sst, sstero, elem);
+	subtract(sstdil, sstero, rfilt);
+
+	laplacian(sst, sstlap);
+
+	clipsst(rfilt);
+
+	cmapImshow("SST", sst, COLORMAP_JET, 0.2);
+	cmapImshow("Rangefilt SST", rfilt, COLORMAP_JET, 0.2);
+	cmapImshow("Laplacian SST", sstlap, COLORMAP_JET, 0.2);
+	cmapImshow("SortIdx Lat", sind, COLORMAP_JET, 0.2);
+	//namedWindow("SortIdx SST", CV_WINDOW_NORMAL|CV_WINDOW_KEEPRATIO);
+	//imshow("SortIdx SST", sind);
+	while(waitKey(0) != 'q')
+		;
 
 	return 0;
 }
